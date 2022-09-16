a3476a736471a7088776b1cdbb16a4554fee3624
MVinnikov <MVinnikov@egartech.ru>
2022-09-16 17:37:14 +0300

    Из файла свойств убрано свойство spring.jackson.default-property-inclusion: non_null так, как оно не работало. Теперь в WebConfig указывается новый конвертер с JsonInclude.Include.NON_NULL. Также убрано ненужное свойство.

--- src/main/java/ru/egartech/sickday/config/WebConfig.java
+++ src/main/java/ru/egartech/sickday/config/WebConfig.java
@@ -1,10 +1,16 @@
 package ru.egartech.sickday.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -19,6 +25,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods("*")
                 .allowedOrigins(receiverUrl);
+    }
 
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        converter.setObjectMapper(mapper);
+        converters.add(converter);
     }
 }
