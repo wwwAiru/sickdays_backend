302a649dd15e154bac814deabc6d1bc26d6f74c3
MVinnikov <MVinnikov@egartech.ru>
2022-09-16 11:43:58 +0300

    Добавлен конфиг для web'а

---
+++ src/main/java/ru/egartech/sickday/config/WebConfig.java
@@ -0,0 +1,24 @@
+package ru.egartech.sickday.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.receiver.url}")
+    private String receiverUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(receiverUrl);
+
+    }
+}
