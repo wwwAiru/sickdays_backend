0292ab92c0c0a93fce612d7a4ac35377eeceed34
MVinnikov <MVinnikov@egartech.ru>
2022-09-16 11:52:25 +0300

    Добавлены файлы свойств для локализации. Также добавлен утилитарный класс для работы с MessageSource.

---
+++ src/main/java/ru/egartech/sickday/util/MessageSourceUtils.java
@@ -0,0 +1,46 @@
+package ru.egartech.sickday.util;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+@Component
+@RequiredArgsConstructor
+public final class MessageSourceUtils {
+
+    private final MessageSource messageSource;
+
+    public String buildMessage(String code,
+                               WebRequest request,
+                               Object... args) {
+        return messageSource.getMessage(code, args, request.getLocale());
+    }
+
+    @RequiredArgsConstructor
+    public enum SickDaysExceptionMessageProperties {
+        BASE("sick-days.exception"),
+        NOT_FOUND(BASE.getName().concat(".not-found")),
+        CREATE(BASE.getName().concat(".create")),
+        DATE_NOT_FOUND(BASE.getName().concat(".not-found-date")),
+        TYPE_NOT_FOUND(BASE.getName().concat(".not-found-type")),
+        STATUS_NOT_FOUND(BASE.getName().concat(".not-found-status")),
+        ;
+
+        @Getter
+        private final String name;
+    }
+
+    @RequiredArgsConstructor
+    public enum EmployeesExceptionMessageProperties {
+        BASE("employee.exception"),
+        NOT_FOUND(BASE.getName().concat(".not-found")),
+        BRANCH_NOT_FOUND(BASE.getName().concat(".not-found-branch")),
+        ;
+
+        @Getter
+        private final String name;
+    }
+
+}
