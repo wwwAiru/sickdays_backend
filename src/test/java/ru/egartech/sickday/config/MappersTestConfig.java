9d93efb08f188ed968dcd15f8386691f357b927a
MVinnikov <MVinnikov@egartech.ru>
2022-09-22 10:18:32 +0300

    Изменены тесты в соответствии с мапперами и сервисом.

---
+++ src/test/java/ru/egartech/sickday/config/MappersTestConfig.java
@@ -0,0 +1,7 @@
+package ru.egartech.sickday.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("ru.egartech.sickday.mapper")
+public class MappersTestConfig extends AbstractBaseConfig {
+}
