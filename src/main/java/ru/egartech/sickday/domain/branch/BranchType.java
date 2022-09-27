37f444d5216b9698c3878d5f467e61328d59e109
MVinnikov <MVinnikov@egartech.ru>
2022-09-27 12:17:28 +0300

    Добавлен метод поиска по названию филиала

--- src/main/java/ru/egartech/sickday/domain/branch/BranchType.java
+++ src/main/java/ru/egartech/sickday/domain/branch/BranchType.java
@@ -1,7 +1,13 @@
 package ru.egartech.sickday.domain.branch;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public enum BranchType {
@@ -15,6 +21,18 @@ public enum BranchType {
     SAMARA("самарский"),
     UNKNOWN("неизвестный"),
     ;
+
+    private static final Map<String, BranchType> BY_NAMES;
+
+    static {
+        BY_NAMES = Arrays
+                .stream(BranchType.values()).
+                collect(Collectors.toMap(BranchType::getAsString, Function.identity()));
+    }
+
+    public static BranchType valueOfName(@NonNull String name) {
+        return BY_NAMES.get(name);
+    }
 
     @Getter
     private final String asString;
